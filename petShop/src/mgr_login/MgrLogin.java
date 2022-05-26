7dbf671738399ef0ad66e83ad91e151ff034e113
DESKTOP-4I93MV9\Ji <ji>
2022-05-26 21:35:33 +0900

    관리자 로그인
    회원 강퇴 기능 추가

--- petShop/src/mgr_login/MgrLogin.java
+++ petShop/src/mgr_login/MgrLogin.java
@@ -49,8 +49,67 @@ public class MgrLogin {
 	
 	
 	public void memberMng() {
+		boolean isCorrect = true; 
 		//회원 관리
+		System.out.println("===== 회원 관리 페이지 =====");
+		System.out.println("1. 회원 조회 및 관리");
+		System.out.println("2. 오늘의 회원 변동");
 		
+		while(isCorrect) {
+			int n = MyUtil.scInt();
+			
+			switch(n) {
+			case 1: memberList(); break;
+			case 2: todayMem(); break;
+			default: System.out.println("상위 메뉴로 돌아갑니다."); isCorrect = false;
+			}
+		}
+		return;
+	}//memberMng
+	
+	
+	private void memberList() {
+		//회원 조회
+		System.out.println("===== 회원 목록 조회 =====");
+		Connection conn = OracleDB.getOracleConnection();
+		String sql = "SELECT * FROM MEMBER WHERE QUIT_YN = 'N' ORDERED BY ENROLL_DATE DESC";
+		
+	}//memberList
+	
+	
+	private void memWithdraw(int mem_no) {
+		//회원 강퇴
+		System.out.println("탈퇴시키겠습니까? (y/n)");
+		String answer = MyUtil.sc.nextLine();
+		if("y".equals(answer)) {
+			Connection conn = OracleDB.getOracleConnection();
+			String sql = "UPDATE MEMBER SET QUIT_YN = 'Y' WHERE MEM_NO = ?";
+			PreparedStatement pstmt = null;
+			try {
+				pstmt = conn.prepareStatement(sql);
+				pstmt.setInt(1, mem_no);
+				int result = pstmt.executeUpdate();
+				if(result == 1) {
+					System.out.println("탈퇴가 성공적으로 이루어졌습니다.");
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}finally {
+				OracleDB.close(conn);
+				OracleDB.close(pstmt);
+			}
+			System.out.println("탈퇴 처리에 실패하였습니다.");
+		}else {
+			System.out.println("상위 메뉴로 돌아갑니다.");
+			return;
+		}
+		
+	}//memWithdraw
+	
+	
+	
+	private void todayMem() {
+		//오늘의 회원 변동 조회
 		
 	}
 
