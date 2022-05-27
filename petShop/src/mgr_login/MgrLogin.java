85fb85b7ba737d55a138888073f1f559b2d1d6a7
DESKTOP-4I93MV9\Ji <ji>
2022-05-27 18:03:05 +0900

    관리자 로그인
    회원 목록 조회 기능(미완)

--- petShop/src/mgr_login/MgrLogin.java
+++ petShop/src/mgr_login/MgrLogin.java
@@ -1,6 +1,7 @@
 package mgr_login;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -71,10 +72,68 @@ public class MgrLogin {
 	private void memberList() {
 		//회원 조회
 		System.out.println("===== 회원 목록 조회 =====");
+		//회원 번호
+		int mem_no;
+		//연결 얻기
 		Connection conn = OracleDB.getOracleConnection();
-		String sql = "SELECT * FROM MEMBER WHERE QUIT_YN = 'N' ORDERED BY ENROLL_DATE DESC";
-		
+		String sql = "SELECT RPAD(MEM_NO, 6,' ') MEM_NO, RPAD(MEM_ID, 20,' ') MEM_ID, RPAD(MEM_PWD, 20,' ') MEM_PWD,"
+				+ " RPAD(NAME, 20,' ') NAME, ENROLL_DATE, QUIT_YN "
+				+ "FROM MEMBER ORDER BY ENROLL_DATE DESC";
+		PreparedStatement pstmt = null;
+		PreparedStatement pstmt2 = null;
+		ResultSet rs = null;
+		ResultSet rs2 = null;
+		try {
+			pstmt = conn.prepareStatement(sql);
+			rs = pstmt.executeQuery();
+			System.out.println("회원번호 |        아이디        |        비밀번호       |         이름        |  회원가입일  | 탈퇴여부");
+			while(rs.next()) {
+				memLook(rs);
+			}
+			int select = 0;
+			while(select != -1) {
+				System.out.print("탈퇴시킬 회원 번호를 선택해 주십시오 (나가기 = -1) : ");
+				select = MyUtil.scInt();
+				String sql2 = "SELECT * FROM MEMBER WHERE MEM_NO = ?";
+				pstmt2 = conn.prepareStatement(sql2);
+				pstmt2.setInt(1, select);
+				rs2 = pstmt2.executeQuery();
+				rs2.next();
+				mem_no = rs2.getInt("MEM_NO");
+				if(!rs2.next()) {
+					System.out.println("해당 값은 존재하지 않습니다.");
+					System.out.println("상위 메뉴로 돌아갑니다.");
+					return;
+				}
+				memWithdraw(mem_no);
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}finally {
+			OracleDB.close(conn);
+			OracleDB.close(rs);
+			OracleDB.close(pstmt);
+			OracleDB.close(rs2);
+		}
 	}//memberList
+
+	
+	
+	private void memLook(ResultSet rs) {
+		try {
+			int mem_no = rs.getInt("MEM_NO");
+			String mem_id = rs.getString("MEM_ID");
+			String mem_pwd = rs.getString("MEM_PWD");
+			String name = rs.getString("NAME");
+			Date enroll_date = rs.getDate("ENROLL_DATE");
+			String quit_yn = rs.getString("QUIT_YN");
+			System.out.println(mem_no + mem_id + mem_pwd + name + enroll_date + quit_yn);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+	}//memLook
 	
 	
 	private void memWithdraw(int mem_no) {
