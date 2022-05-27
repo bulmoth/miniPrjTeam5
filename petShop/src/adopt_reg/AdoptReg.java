afe52b032400def7da48fc243fcbf632eb0d0204
INTEL-PC\Intel <ji>
2022-05-28 04:07:24 +0900

    관리자 로그인/입양 등록
    현재 로그인한 관리자 아이디 static 변수로 설정
    관리자 입양 등록 기능 완성

--- petShop/src/adopt_reg/AdoptReg.java
+++ petShop/src/adopt_reg/AdoptReg.java
@@ -5,6 +5,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import mgr_login.MgrLogin;
 import oracleDB.OracleDB;
 import util.MyUtil;
 
@@ -18,7 +19,7 @@ public class AdoptReg {
 		while(!isQuit) {
 			
 			//입양 등록
-			System.out.println("========== 입양 ==========");
+			System.out.println("========== 입양(관리자) ==========");
 			System.out.println();
 			System.out.println("입양 관련 페이지 입니다.");
 			System.out.println("원하시는 기능을 선택하십시오.");
@@ -80,7 +81,7 @@ public class AdoptReg {
 		System.out.println("========== 입양 완료 목록 ==========");
 		Connection conn = OracleDB.getOracleConnection();
 		String sql = "SELECT RPAD(TO_CHAR(ADT_NO), 6, ' ') ADT_NO, LPAD(ADM_ID, 21, ' ') ADM_ID, "
-				+ "LPAD(TYPE, 5, ' ') TYPE, LPAD(ANI_NAME, 25, ' ') ANI_NAME "
+				+ "LPAD(TYPE, 4, ' ') TYPE, LPAD(ANI_NAME, 25, ' ') ANI_NAME "
 				+ "FROM ADOPT WHERE ADOPT = 'Y' ORDER BY ADT_NO ASC";
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
@@ -107,6 +108,36 @@ public class AdoptReg {
 	
 	private void register() {
 		//입양 등록
+		System.out.println("======== 입양 등록(관리자) ========");
+		Connection conn = OracleDB.getOracleConnection();
+		String sql = "INSERT INTO ADOPT(ADT_NO, MEM_ID, TYPE, ANI_NAME) VALUES(ADOPT_SEQ.NEXTVAL, ?, ?, ?)";
+		PreparedStatement pstmt = null;
+		try {
+			pstmt = conn.prepareStatement(sql);
+			System.out.println("입양 등록을 시작합니다.");
+			System.out.print("동물 종을 입력하세요(개 : D|고양이 : C|그 외 : 공란) : ");
+			String type = MyUtil.sc.nextLine();
+			pstmt.setString(2, MgrLogin.mgrIdNow);
+			pstmt.setString(1, type);
+			System.out.print("동물 이름을 입력하세요 : ");
+			String ani_name = MyUtil.sc.nextLine();
+			pstmt.setString(3, ani_name);
+			int result = pstmt.executeUpdate();
+			if(result == 1) {
+				System.out.println("등록을 성공적으로 마쳤습니다.");
+				System.out.println("상위 메뉴로 돌아갑니다.");
+				return;
+			}else {
+				System.out.println("등록에 실패하였습니다.");
+				System.out.println("상위 메뉴로 돌아갑니다.");
+				return;
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}finally {
+			OracleDB.close(conn);
+			OracleDB.close(pstmt);
+		}
 		
 	}//register
 	
