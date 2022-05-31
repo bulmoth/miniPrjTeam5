80e0c6e7419aea0ed1b0ad515828b6ad1ec64687
Giggs <gwiseok22@naver.com>
2022-05-31 16:21:33 +0900

    끝 ㅜㅜ

--- petShop/src/mgr_checkOrderList/CheckOrderList.java
+++ petShop/src/mgr_checkOrderList/CheckOrderList.java
@@ -19,7 +19,7 @@ public class CheckOrderList {
 		while (true) {
 			System.out.println("============================");
 			System.out.println();
-			System.out.println("1. 오늘의 주문서 추가하기");
+			System.out.println("1. 오늘의 주문서 확인하기");
 			System.out.println("2. 오늘의 판매 수량 확인");
 			System.out.println("3. 회원별 구매 수량 확인");
 			System.out.println("4. 관리자 메뉴로 돌아가기");
@@ -46,24 +46,105 @@ public class CheckOrderList {
 			}
 		} // while
 	}
+	
+	
+	//오늘의 주문서 가져오기 
+		public void checkSale() {
+			
+					
+			//안내 문구 출력
+			//입력 받기 ( 제목, 내용)
+			System.out.println("======== 오늘의 주문을 확인합니다.  ========");
+			
+			Connection conn = OracleDB.getOracleConnection();
+			
+//			2. SELECT 쿼리 날리기
+			String sql = "SELECT mem_no, ord_no, ord_cnt, order_date FROM ORDER_TBL";
+			String sql2 = "SELECT mem_no, ord_no, ord_cnt, order_date, PRICE * ord_cnt AS PRICE FROM ORDER_TBL JOIN PRODUCT USING(PRD_NO)";
+			
+			
+			PreparedStatement pstmt = null;
+			ResultSet rs = null;
+			
+			
+			
+				try {
+					pstmt = conn.prepareStatement(sql2);
+					
+					rs = pstmt.executeQuery();
+					
+					System.out.print("회원 번호");
+					System.out.print(" | ");
+					System.out.print("주문 번호");
+					System.out.print(" | ");
+					System.out.print("주문 품목");
+					System.out.print(" | ");
+					System.out.print("주문 총액");
+					System.out.print(" | ");
+					System.out.print("주문 날짜");
+					System.out.println("\n-----------------------------");
+					
+					
+					while(rs.next()) {
+						
+						int no = rs.getInt("mem_no"); //회원 번호
+						int no2 = rs.getInt("ord_no"); //주문 번호
+						int no3 = rs.getInt("ord_cnt"); //주문 수량
+						int no4 = rs.getInt("PRICE");
+						Date rs2 = rs.getDate("order_date"); //주문 날짜
 
+
+						
+						System.out.print(no);
+						System.out.print(" | ");
+						System.out.print(no2);
+						System.out.print(" | ");
+						System.out.print(no3);
+						System.out.print(" | ");
+						System.out.print(no4);
+						System.out.print(" | ");
+						System.out.print(rs2);
+						System.out.println();		
+						
+						
+						
+					}
+					System.out.println("오늘의 매출을 확인하시겠습니까? (Y/N)");
+					
+					String answer = MyUtil.sc.nextLine();
+					if("Y".equalsIgnoreCase(answer)) {
+					checkSale2();
+					}
+					
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}finally {
+					OracleDB.close(conn);
+					OracleDB.close(pstmt);
+					OracleDB.close(rs);
+					
+					
+				}
+						
+				
+		}//checkSale
 	
 //	
 	
-	//오늘의 주문서 가져오기 
-	public void checkSale() {
+	//오늘의 총 매출액
+	public void checkSale2() {
 		
 				
-		//안내 문구 출력
-		//입력 받기 ( 제목, 내용)
-		System.out.println("======== 오늘의 주문을 확인합니다.  ========");
+		
+		System.out.println("======== 오늘의 매출액을 확인합니다.  ========");
 		
 		Connection conn = OracleDB.getOracleConnection();
+				
 		
-//		2. SELECT 쿼리 날리기
-//		String sql = "SELECT mem_no, ord_no, ord_cnt, order_date FROM ORDER_TBL";
+		String sql = "SELECT SUM((ORDER_TBL.ORD_CNT)*(PRODUCT.PRICE)) AS TOTALPRICE FROM ORDER_TBL, PRODUCT WHERE ORDER_TBL.PRD_NO = PRODUCT.PRD_NO";
+				
 		
-		String sql = "SELECT mem_no, ord_no, ord_cnt, order_date, PRICE * ord_cnt AS PRICE FROM ORDER_TBL JOIN PRODUCT USING(PRD_NO)";
+		
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
 		
@@ -74,37 +155,17 @@ public class CheckOrderList {
 				
 				rs = pstmt.executeQuery();
 				
-				System.out.print("회원 번호");
-				System.out.print(" | ");
-				System.out.print("주문 번호");
-				System.out.print(" | ");
-				System.out.print("주문 수량");
-				System.out.print(" | ");
-				System.out.print("주문 금액");
-				System.out.print(" | ");
-				System.out.print("주문 날짜");
-				System.out.println("\n-----------------------------");
+				System.out.print("오늘의 총 매출액은 ::  ");
+
 				
 				
 				while(rs.next()) {
 					
-					int no = rs.getInt("mem_no"); //회원 번호
-					int no2 = rs.getInt("ord_no"); //주문 번호
-					int no3 = rs.getInt("ord_cnt"); //주문 수량
-					int no4 = rs.getInt("PRICE");
-					Date rs2 = rs.getDate("order_date"); //주문 날짜
+
+					int no = rs.getInt("TOTALPRICE"); //회원 번호
+
 					
-					
-					System.out.print(no);
-					System.out.print(" | ");
-					System.out.print(no2);
-					System.out.print(" | ");
-					System.out.print(no3);
-					System.out.print(" | ");
-					System.out.print(no4);
-					System.out.print(" | ");
-					System.out.print(rs2);
-					System.out.println();		
+					System.out.print(no + "원 입니다.");
 				
 				}
 				
@@ -132,19 +193,20 @@ public class CheckOrderList {
 		
 		Connection conn = OracleDB.getOracleConnection();
 		
-		String sql = "SELECT SUM(ORD_CNT) FROM ORDER_TBL";
-		String sql2 = "SELECT PRD_NO, PRICE FROM PRODUCT";
+		String sql = "SELECT SUM(ORD_CNT) AS COUNT FROM ORDER_TBL WHERE TO_DATE(ORDER_DATE) = ?";
+		
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
-		
-		PreparedStatement pstmt2 = null;
+	
 		
 		try {
 			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, inputDate);
 			rs = pstmt.executeQuery();
-			pstmt2 = conn.prepareStatement(sql2);
+			
+			
 			if(rs.next()) {
-				int totalCount = rs.getInt("SUM(ORD_CNT)");
+				int totalCount = rs.getInt("COUNT");
 				
 				
 				System.out.println(inputDate + " 일의 총 주문 수량은" + totalCount + " 입니다");
@@ -175,8 +237,7 @@ public class CheckOrderList {
 		int inputMember = MyUtil.scInt();
 		
 		Connection conn = OracleDB.getOracleConnection();
-		//넘버에 봐서 회원번호로할지 물건번호로 할지 체크
-		String sql = "SELECT SUM(ord_cnt) FROM ORDER_TBL";
+		String sql = "SELECT ord_cnt FROM ORDER_TBL WHERE MEM_NO = ?";
 
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
@@ -184,11 +245,11 @@ public class CheckOrderList {
 		
 		try {
 			pstmt = conn.prepareStatement(sql);
-			
+			pstmt.setInt(1, inputMember);
 			rs = pstmt.executeQuery();
 			
 			if(rs.next()) {
-				int totalCount = rs.getInt("SUM(ORD_CNT)");
+				int totalCount = rs.getInt("ORD_CNT");
 				
 				System.out.println(inputMember + " 번 회원의 총 주문 수량은 " + totalCount + " 입니다.");
 			}
